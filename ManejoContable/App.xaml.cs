f4f56463c8a60611337ffcb93eb73b58af606f24
Andres_asus <andres.hoyos18@eia,edu.co>
2022-07-18 10:53:37 -0500

    Created Configuration Property

--- ManejoContable/App.xaml.cs
+++ ManejoContable/App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DbContextLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ManejoContable
 {
@@ -13,5 +17,39 @@ namespace ManejoContable
     /// </summary>
     public partial class App : Application
     {
+        private const string DbNameString = "SomeName"; // TODO: Define database name
+        private static IConfigurationRoot? _config;
+        private static DbContextOptionsBuilder<ContabilidadDbContext>? _dbContextOptionsBuilder;
+
+        public static DbContextOptions<ContabilidadDbContext> DbContextOptions
+        {
+            get
+            {
+                if (_config == null || _dbContextOptionsBuilder == null)
+                {
+                    CreateConfigurationRoot();
+                }
+
+                return _dbContextOptionsBuilder!.Options;
+            }
+        }
+
+        public App()
+        {
+            CreateConfigurationRoot();
+        }
+
+        private static void CreateConfigurationRoot()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _config = builder.Build();
+
+            _dbContextOptionsBuilder = new DbContextOptionsBuilder<ContabilidadDbContext>();
+            _dbContextOptionsBuilder.UseSqlServer(_config.GetConnectionString(DbNameString));
+
+            // return _dbContextOptionsBuilder.Options;
+        }
     }
 }
