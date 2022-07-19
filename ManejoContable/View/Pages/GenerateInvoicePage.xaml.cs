ab9f71b914dc7dca9a9604db3240b23dedb4fe31
andre <andres.hoyos18@eia.edu.co>
2022-07-19 17:17:27 -0500

    Added Page for invoice

---
+++ ManejoContable/View/Pages/GenerateInvoicePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManejoContable.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for FacturaPage.xaml
+    /// </summary>
+    public partial class FacturaPage : Page
+    {
+        public FacturaPage()
+        {
+            InitializeComponent();
+
+            TextBlock.Text = $"This it the {nameof(FacturaPage)}";
+
+        }
+    }
+}
