febe860aaca145f6c5d2484e68f548d9bca6697c
andre <andres.hoyos18@eia.edu.co>
2022-07-19 23:02:43 -0500

    Created the ProductsDetailControl to display the product details of a Invoice

---
+++ ManejoContable/UserControls/FacturaProductosDetailsControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ModelEntities;
+
+namespace ManejoContable.UserControls
+{
+    /// <summary>
+    /// Interaction logic for FacturaProductosDetailsControl.xaml
+    /// </summary>
+    public partial class FacturaProductosDetailsControl : UserControl
+    {
+
+        private readonly List<ProductoFactura> productoList = new List<ProductoFactura>();
+        public FacturaProductosDetailsControl()
+        {
+            InitializeComponent();
+
+            for (var i = 0; i < 10; i++)
+            {
+                productoList.Add(new ProductoFactura());
+            }
+
+            ProductsGrid.ItemsSource = productoList;
+
+        }
+
+    }
+}
