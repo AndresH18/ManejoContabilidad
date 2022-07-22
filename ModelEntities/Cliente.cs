b61ff465f2a89ef7a29c180d07b7bcbd56b22655
Andres_a <andres.hoyos18@eia,edu.co>
2022-07-22 10:46:11 -0500

    Created Client Entity

---
+++ ModelEntities/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace ModelEntities;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string TipoDocumento { get; set; } = "NIT";
+    public string NumeroDocumento { get; set; } = "123 233 9888";
+    public string Direccion { get; set; }
+    public int MunicipioId { get; set; }
+    public string Correo { get; set; }
+    public string Telefono { get; set; }
+    public string TipoContribuyente { get; set; }
+    public string RegimenContable { get; set; }
+    public string Responsabilidad { get; set; }
+}
