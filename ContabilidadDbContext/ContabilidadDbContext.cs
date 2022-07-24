06ac1a38b2850ae7d2b3629bb0fe07f9cb1c1c09
andre <andres.hoyos18@eia.edu.co>
2022-07-24 16:51:25 -0500

    Created Migration; Added Cliente Model to EFcore

--- ContabilidadDbContext/ContabilidadDbContext.cs
+++ ContabilidadDbContext/ContabilidadDbContext.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using ModelEntities;
 
 namespace DbContextLibrary
 {
     public class ContabilidadDbContext : DbContext
     {
-        private const string DbName = "DatabaseName"; // TODO: Define database name
-        
+        private const string DbName = "Contabilidad"; // TODO: Define database name
+
         private static IConfigurationRoot? _configuration;
         private static DbContextOptionsBuilder<ContabilidadDbContext>? _dbContextOptionsBuilder;
 
+        public DbSet<Cliente> Clientes { get; set; }
 
         public ContabilidadDbContext()
         {
@@ -32,6 +34,9 @@ namespace DbContextLibrary
                 _configuration = builder.Build();
 
                 var connectionString = _configuration.GetConnectionString(DbName);
+                if (connectionString == null)
+                    throw new Exception(
+                        $"Connection string is null. Directory={Directory.GetCurrentDirectory()}, File Exists={File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))}");
 
                 if (connectionString != null)
                 {
