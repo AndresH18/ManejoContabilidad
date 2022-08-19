f7bf7a671da9a20ed4895bced7e426a7c46f5a46
Andres_a <andres.hoyos18@eia.edu.co>
2022-08-19 11:07:40 -0500

    Created Api Controllers for Cliente, Marca, Categoria

---
+++ WebApi/Controllers/ClientesRepository.cs
@@ -0,0 +1,71 @@
+﻿using DbContextLibrary.Repository;
+using Microsoft.AspNetCore.Mvc;
+using ModelEntities;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class ClientesRepository : Controller
+{
+    private IClienteRepository _repository;
+
+    public ClientesRepository()
+    {
+        _repository = new ClienteRepository();
+    }
+
+    public ClientesRepository(IClienteRepository repository)
+    {
+        _repository = repository;
+    }
+
+    // GET
+    [HttpGet]
+    public ActionResult<List<Cliente>> GetAll() => _repository.GetAll().ToList();
+
+    [HttpGet("{id}")]
+    public ActionResult<Cliente> Get(int id)
+    {
+        var cliente = _repository.GetById(id);
+        if (cliente == null)
+            return NotFound();
+
+        return Ok(cliente);
+    }
+
+    // POST
+    [HttpPost]
+    public ActionResult Create(Cliente cliente)
+    {
+        _repository.Create(cliente);
+        return CreatedAtAction(nameof(Create), new { cliente.Id }, cliente);
+    }
+
+    // PUT
+    [HttpPut("{id}")]
+    public ActionResult Update(int id, Cliente cliente)
+    {
+        if (id != cliente.Id)
+            return BadRequest();
+
+        var existingCliente = _repository.GetById(id);
+        if (existingCliente == null)
+            return NotFound();
+
+        _repository.Update(existingCliente);
+        return NoContent();
+    }
+
+    // DELETE
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var cliente = _repository.GetById(id);
+        if (cliente == null)
+            return NotFound();
+
+        _repository.Delete(id);
+        return NoContent();
+    }
+}
---
+++ WebApi/Controllers/MarcasController.cs
@@ -0,0 +1,74 @@
+﻿using DbContextLibrary.Repository;
+using Microsoft.AspNetCore.Mvc;
+using ModelEntities;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class MarcasController : Controller
+{
+    private IMarcaRepository _repository;
+
+    public MarcasController()
+    {
+        _repository = new MarcaRepository();
+    }
+
+    public MarcasController(IMarcaRepository repository)
+    {
+        _repository = repository;
+    }
+
+    // GET
+    [HttpGet]
+    public ActionResult<List<Marca>> GetAll() => _repository.GetAll().ToList();
+
+    [HttpGet("{id}")]
+    public ActionResult<Marca> Get(int id)
+    {
+        var marca = _repository.GetById(id);
+        if (marca == null)
+            return NotFound();
+
+        return Ok(marca);
+    }
+
+    // POST
+    [HttpPost]
+    public ActionResult Create(Marca marca)
+    {
+        _repository.Create(marca);
+        return CreatedAtAction(nameof(Create), new { marca.Id }, marca);
+    }
+
+    // PUT
+    [HttpPut("{id}")]
+    public ActionResult Update(int id, Marca marca)
+    {
+        if (id != marca.Id)
+            return BadRequest();
+
+        var existingMarca = _repository.GetById(marca.Id);
+        if (existingMarca == null)
+            return NotFound();
+
+        _repository.Update(marca);
+
+        return NoContent();
+    }
+
+    // DELETE   
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var existingMarca = _repository.GetById(id);
+        if (existingMarca == null)
+        {
+            return NotFound();
+        }
+
+        _repository.Delete(id);
+        return NoContent();
+    }
+}
