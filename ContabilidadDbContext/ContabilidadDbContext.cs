fbca5daeb83cb7474a2680e6187582eafc6e457b
Andres_a <andres.hoyos18@eia.edu.co>
2022-08-19 11:06:16 -0500

    Created Repository Pattern for accessing DB

--- ContabilidadDbContext/ContabilidadDbContext.cs
+++ ContabilidadDbContext/ContabilidadDbContext.cs
@@ -3,75 +3,75 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Configuration;
 using ModelEntities;
 
-namespace DbContextLibrary
+namespace DbContextLibrary;
+
+public class ContabilidadDbContext : DbContext
 {
-    public class ContabilidadDbContext : DbContext
+    private const string DbName = "Contabilidad"; // TODO: Define database name
+
+    private static IConfigurationRoot? _configuration;
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Marca> Marcas { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+
+    public ContabilidadDbContext()
     {
-        private const string DbName = "Contabilidad"; // TODO: Define database name
+    }
 
-        private static IConfigurationRoot? _configuration;
+    public ContabilidadDbContext(DbContextOptions options) : base(options)
+    {
+    }
 
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Producto> Productos { get; set; }
 
-        public ContabilidadDbContext()
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
         {
-        }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
-        public ContabilidadDbContext(DbContextOptions options) : base(options)
-        {
-        }
+            _configuration = builder.Build();
 
+            var connectionString = _configuration.GetConnectionString(DbName);
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // base.OnConfiguring(optionsBuilder);
-            if (!optionsBuilder.IsConfigured)
+            if (connectionString != null)
             {
-                var builder = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-                _configuration = builder.Build();
-
-                var connectionString = _configuration.GetConnectionString(DbName);
-
-                if (connectionString != null)
-                {
-                    optionsBuilder.UseSqlServer(connectionString);
-                }
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Cliente>()
-                .Property(c => c.TipoDocumento)
-                .HasConversion<EnumToStringConverter<TipoDocumento>>();
-
-            modelBuilder.Entity<Cliente>().HasData(
-                new Cliente
-                {
-                    Id = 1, Nombre = "Imporcom", NumeroDocumento = "123-456-7890", TipoDocumento = TipoDocumento.Nit
-                },
-                new Cliente
-                {
-                    Id = 2, Nombre = "Andres' Programmers SAS", NumeroDocumento = "111-222-33-44"
-                }
-            );
-        }
-
-
-        // private void CreateConfigurationRoot()
-        // {
-        //     var builder = new ConfigurationBuilder()
-        //         .SetBasePath(Directory.GetCurrentDirectory())
-        //         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-        //     _configuration = builder.Build();
-        //
-        //     _dbContextOptionsBuilder = new DbContextOptionsBuilder<ContabilidadDbContext>();
-        //     _dbContextOptionsBuilder.UseSqlServer(_configuration.GetConnectionString(DbName));
-        // }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.TipoDocumento)
+            .HasConversion<EnumToStringConverter<TipoDocumento>>();
+
+        modelBuilder.Entity<Cliente>().HasData(
+            new Cliente
+            {
+                Id = 1, Nombre = "Imporcom", NumeroDocumento = "123-456-7890", TipoDocumento = TipoDocumento.Nit
+            },
+            new Cliente
+            {
+                Id = 2, Nombre = "Andres' Programmers SAS", NumeroDocumento = "111-222-33-44"
+            }
+        );
+    }
+
+
+    // private void CreateConfigurationRoot()
+    // {
+    //     var builder = new ConfigurationBuilder()
+    //         .SetBasePath(Directory.GetCurrentDirectory())
+    //         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+    //     _configuration = builder.Build();
+    //
+    //     _dbContextOptionsBuilder = new DbContextOptionsBuilder<ContabilidadDbContext>();
+    //     _dbContextOptionsBuilder.UseSqlServer(_configuration.GetConnectionString(DbName));
+    // }
 }
---
+++ ContabilidadDbContext/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DbContextLibrary.Repository;
+
+public interface IRepository<T>
+{
+    T Create(T entity);
+    IEnumerable<T> GetAll();
+    T? GetById(int id);
+    void Update(T entity);
+    void Delete(int id);
+}
