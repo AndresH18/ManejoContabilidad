9f2a4fd00c8ff1dad9586071112972cc52eacd36
Andres_a <andres.hoyos18@eia.edu.co>
2022-08-19 11:27:48 -0500

    Added Dependency Injection

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,13 @@
+using DbContextLibrary.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// TODO: Add Services for Dependency Injection
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<IMarcaRepository, MarcaRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
