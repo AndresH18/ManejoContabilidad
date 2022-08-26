ab4b9cb91f6ed6fd0f7cd9d7bb2ef47c4819bc1e
Andres_a <andres.hoyos18@eia.edu.co>
2022-08-26 09:02:35 -0500

    Added Testing button

--- ContabilidadWinUI/App.xaml.cs
+++ ContabilidadWinUI/App.xaml.cs
@@ -42,10 +42,10 @@ namespace ContabilidadWinUI
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            _mainWindow = new MainWindow();
+            _mainWindow.Activate();
         }
 
-        private Window m_window;
+        private Window _mainWindow;
     }
 }
--- ContabilidadWinUI/View/Categoria/CategoriasPage.xaml.cs
+++ ContabilidadWinUI/View/Categoria/CategoriasPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,9 +11,17 @@ namespace ContabilidadWinUI.View.Categoria
     /// </summary>
     public sealed partial class CategoriasPage : Page
     {
+        private int _counter = 0;
+
         public CategoriasPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            Button.Content = _counter == 1 ? $"Clicked {_counter} time" : $"Clicked {_counter} times";
         }
     }
 }
