c30c515eef39622effe30dff4cb6b35fd40320ed
andre <andres.hoyos18@eia.edu.co>
2022-08-28 21:04:09 -0500

    Added Dependency Injection for controller's repositories objects

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,19 @@
+using DbContextLibrary.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// https://docs.microsoft.com/en-us/dotnet/core/extensions/dependency-injection#service-lifetimes
+// Dependency Injection
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IDetallesFacturaRepository, DetallesFacturaRepositoryRepository>();
+builder.Services.AddScoped<IFacturaRepository, FacturaRepository>();
+builder.Services.AddScoped<IInfoFacturaRepository, InfoFacturaRepository>();
+builder.Services.AddScoped<IMarcaRepository, MarcaRepository>();
+builder.Services.AddScoped<IProductoFacturaRepository, ProductoFacturaRepository>();
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
