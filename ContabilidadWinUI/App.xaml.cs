407ffb7761d2c2d78391016958d7b39128385f5e
andre <andres.hoyos18@eia.edu.co>
2022-09-11 21:43:25 -0500

    Revert "Added Dependency Injection Packages, created Service Provider"
    This reverts commit de440705f4b2a327e2f2996b261e3f1d4a2bc288.

--- ContabilidadWinUI/App.xaml.cs
+++ ContabilidadWinUI/App.xaml.cs
@@ -15,9 +15,6 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using DbContextLibrary;
-using DbContextLibrary.Repository;
-using Microsoft.Extensions.DependencyInjection;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,8 +32,6 @@ namespace ContabilidadWinUI
         /// </summary>
         public App()
         {
-            Services = ConfigureServices();
-
             this.InitializeComponent();
         }
 
@@ -52,31 +47,5 @@ namespace ContabilidadWinUI
         }
 
         public Window? Window { get; private set; }
-
-        /// <summary>
-        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
-        /// </summary>
-        public IServiceProvider Services { get; }
-
-        /// <summary>
-        /// Gets the current <see cref="App"/> instance in use
-        /// </summary>
-        public new static App Current => (App) Application.Current;
-
-
-        /// <summary>
-        /// Configures the services for the application.
-        /// </summary>
-        private static IServiceProvider ConfigureServices()
-        {
-            var services = new ServiceCollection();
-
-            // Add Services Here
-            services.AddDbContext<ContabilidadDbContext>();
-
-            services.AddScoped<IClienteRepository, ClienteRepository>();
-
-            return services.BuildServiceProvider();
-        }
     }
 }
