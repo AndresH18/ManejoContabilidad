506bf4af0a597002763e3f07ac6b2180b278d13e
Andres_a <andres.hoyos18@eia.edu.co>
2022-09-16 11:16:43 -0500

    Created FacturasService, using FacturaDto
    Gets the Factura related information for the DataGrid

---
+++ ContabilidadWinUI/Services/FacturasService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DbContextLibrary;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContabilidadWinUI.Services;
+
+public interface IFacturasService
+{
+    IEnumerable<FacturaDto> GetAllFacturas();
+}
+
+public class FacturasService : IFacturasService
+{
+    private readonly ContabilidadDbContext _db;
+
+    public FacturasService(ContabilidadDbContext db)
+    {
+        _db = db;
+    }
+
+    public IEnumerable<FacturaDto>
+        GetAllFacturas()
+    {
+        var s = _db.Facturas.Include(f => f.Cliente).Include(f => f.InfoFactura).ToList();
+
+        return (from entry in s
+            select new FacturaDto(entry.Id, entry.InfoFactura!.NumeroFactura, entry.ClienteId, entry.Cliente!.Nombre,
+                entry.Cliente.NumeroDocumento)).ToList();
+        //
+        // List<(int facturaId, string? numeroFactura, string nombreCliente, string documentoCliente)> tuples =
+        //     (from entry in s
+        //         select (entry.Id,
+        //             entry.InfoFactura!.NumeroFactura,
+        //             entry.Cliente!.Nombre,
+        //             entry.Cliente.NumeroDocumento)
+        //     ).ToList();
+        //
+        // return tuples;
+    }
+}
+
+public class FacturaDto
+{
+    public int Id { get; set; }
+    public string? NumeroFactura { get; set; }
+    public int ClienteId { get; set; }
+    public string NombreCliente { get; set; }
+    public string DocumentoCliente { get; set; }
+
+    public FacturaDto(int id, string? numeroFactura, int clienteId, string nombreCliente, string documentoCliente)
+    {
+        Id = id;
+        NumeroFactura = numeroFactura;
+        ClienteId = clienteId;
+        NombreCliente = nombreCliente;
+        DocumentoCliente = documentoCliente;
+    }
+}
