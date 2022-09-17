ccd14e35405415bac14262966cc026fc1d3ef903
andre <andres.hoyos18@eia.edu.co>
2022-09-16 22:28:24 -0500

    Testing some Type methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+Persona p = new Persona();
+Estudiante e = new Estudiante();
+
+Persona pe = new Estudiante();
+
+Console.WriteLine(p.GetType() == typeof(Persona));
+Console.WriteLine(e.GetType() == typeof(Estudiante));
+Console.WriteLine(pe.GetType().IsAssignableFrom(typeof(Persona)));
+Console.WriteLine(pe.GetType().IsAssignableTo(typeof(Persona)));
+Console.WriteLine(pe.GetType() == typeof(Estudiante));
+
+class Persona
+{
+}
+
+class Estudiante : Persona
+{
+}
