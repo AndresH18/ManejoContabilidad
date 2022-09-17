c04c6d5b7da508d1d726d58d543f61068f195814
Andres_a <andres.hoyos18@eia.edu.co>
2022-09-17 11:48:21 -0500

    Started controller

---
+++ WebApi.Tests/MarcasControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using DbContextLibrary;
+using DbContextLibrary.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelEntities;
+using Moq;
+using WebApi.Controllers;
+
+namespace WebApi.Tests;
+
+public class MarcasControllerTests
+{
+    private readonly AbstractController<IMarcaRepository, Marca> _controller;
+
+    public MarcasControllerTests()
+    {
+        var db = ContabilidadDbContext.InMemoryDatabase();
+        var repo = new MarcaRepository(db);
+        _controller = new MarcasController(repo);
+    }
+
+    [Fact]
+    public void GetAll_ReturnsListOfMarca_True()
+    {
+        // arrange
+        var actionResult = _controller.GetAll();
+
+        // act
+        var value = actionResult.Value;
+
+        // assert
+        Assert.IsAssignableFrom<List<Marca>>(value);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    public void GetById_Exists_False(int id)
+    {
+        // arrange
+        var actionResult = _controller.Get(id);
+        
+        // act
+        var result = actionResult.Result;
+        // assert
+        
+        
+    }
+}
---
+++ WebApi/Controllers/IController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public interface IController<T>
+{
+    ActionResult<List<T>> GetAll();
+    ActionResult<T?> Get(int id);
+    ActionResult Update(int id, T t);
+    ActionResult Delete(int id);
+}
