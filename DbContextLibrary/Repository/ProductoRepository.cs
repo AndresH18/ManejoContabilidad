59478ce6ecd220a0f8e569894b99f674ab2bd60c
andre <andres.hoyos18@eia.edu.co>
2022-09-20 20:11:56 -0500

    Added async variant to methods

--- DbContextLibrary/Repository/ProductoRepository.cs
+++ DbContextLibrary/Repository/ProductoRepository.cs
@@ -24,12 +24,28 @@ public class ProductoRepository : IProductoRepository
         return entity;
     }
 
+    public async Task<Producto> CreateAsync(Producto entity)
+    {
+        entity.Id = 0;
+        await _db.Productos.AddAsync(entity);
+        await _db.SaveChangesAsync();
+        return entity;
+    }
+
     public IEnumerable<Producto> GetAll()
     {
         return _db.Productos
             .Include(p => p.Categoria)
             .Include(p => p.Marca)
             .ToList();
+    }
+
+    public async Task<IEnumerable<Producto>> GetAllAsync()
+    {
+        return await _db.Productos
+            .Include(p => p.Categoria)
+            .Include(p => p.Marca)
+            .ToListAsync();
     }
 
     public Producto? GetById(int id)
@@ -40,10 +56,24 @@ public class ProductoRepository : IProductoRepository
             .FirstOrDefault(p => p.Id == id);
     }
 
+    public async Task<Producto?> GetByIdAsync(int id)
+    {
+        return await _db.Productos
+            .Include(p => p.Categoria)
+            .Include(p => p.Marca)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
     public void Update(Producto entity)
     {
         _db.Productos.Update(entity);
         _db.SaveChanges();
+    }
+
+    public async Task UpdateAsync(Producto entity)
+    {
+        _db.Productos.Update(entity);
+        await _db.SaveChangesAsync();
     }
 
     public void Delete(int id)
@@ -53,6 +83,16 @@ public class ProductoRepository : IProductoRepository
         {
             _db.Productos.Remove(producto);
             _db.SaveChanges();
+        }
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var producto = await GetByIdAsync(id);
+        if (producto != null)
+        {
+            _db.Productos.Remove(producto);
+            await _db.SaveChangesAsync();
         }
     }
 }
