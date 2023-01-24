8d73ec240715ba7f22a337ecbf0c735251400b4e
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-01-23 20:15:49 -0500

    Fixed Invoice.Path not setting not clearing

---
+++ ManejoContabilidad.Wpf/Converters/ObjectNullConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ManejoContabilidad.Wpf.Converters;
+
+public class ObjectNullConverter : IValueConverter
+{
+    /// <summary>
+    /// Returns <paramref name="value"/>
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns>Return <paramref name="value"/></returns>
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+    /// <summary>
+    /// Returns <b>null</b>
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="targetType"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns><b>null</b></returns>
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
--- ManejoContabilidad.Wpf/Views/Invoice/InvoiceWindow.xaml.cs
+++ ManejoContabilidad.Wpf/Views/Invoice/InvoiceWindow.xaml.cs
@@ -1,34 +1,30 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using Models = Shared.Models;
-
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
 
 namespace ManejoContabilidad.Wpf.Views.Invoice;
 
 /// <summary>
-/// Interaction logic for InvoiceWindow.xaml
+///     Interaction logic for InvoiceWindow.xaml
 /// </summary>
 [INotifyPropertyChanged]
 public partial class InvoiceWindow : Window
 {
-    private string FileFilter = "PDF (*.pdf)|*.pdf|All Files (*.*)|*.*";
-    public string? FilePath => Invoice.Path;
-    public bool IsReadOnly { get; }
-    public Models::Invoice Invoice { get; private set; }
+    private const string FileFilter = "PDF (*.pdf)|*.pdf|All Files (*.*)|*.*";
 
-    public InvoiceWindow(Models::Invoice? invoice = null, int invoiceNumber = 0, bool isReadOnly = false)
+    public InvoiceWindow(Shared.Models.Invoice? invoice = null, int invoiceNumber = 0)
     {
-        IsReadOnly = isReadOnly;
-        Invoice = invoice?.Clone() ?? new Models::Invoice { InvoiceNumber = invoiceNumber };
+        Invoice = invoice?.Clone() ?? new Shared.Models.Invoice { InvoiceNumber = invoiceNumber };
 
         InitializeComponent();
 
         Owner = App.Current.MainWindow;
     }
+
+    public Shared.Models.Invoice Invoice { get; }
 
     [RelayCommand]
     private void SearchFile()
@@ -37,7 +33,7 @@ public partial class InvoiceWindow : Window
         {
             Filter = FileFilter,
             Multiselect = false,
-            InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
         };
 
         if (fileDialog.ShowDialog(this) != true)
@@ -45,16 +41,12 @@ public partial class InvoiceWindow : Window
         var path = fileDialog.FileName;
 
         Invoice.Path = path;
-        OnPropertyChanged(nameof(FilePath));
     }
 
     [RelayCommand]
     private void Confirm()
     {
-        if (Verify())
-        {
-            DialogResult = true;
-        }
+        if (Verify()) DialogResult = true;
     }
 
     [RelayCommand]
@@ -78,9 +70,9 @@ public partial class InvoiceWindow : Window
     }
 
     /// <summary>
-    /// Verifies that the annotations on <see cref="Invoice"/> are Valid
+    ///     Verifies that the annotations on <see cref="Invoice" /> are Valid
     /// </summary>
-    /// <returns><b>true</b> if <see cref="Invoice"/> is valid; <b>false</b> otherwise</returns>
+    /// <returns><b>true</b> if <see cref="Invoice" /> is valid; <b>false</b> otherwise</returns>
     private bool VerifyModel()
     {
         return Invoice.IsValid();
