8076805f196f302f96ef8926460f96af8c357e08
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-04-08 02:50:29 -0500

    .

---
+++ AdminAssist.wpf/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AdminAssist.wpf.Services;
+using AdminAssist.wpf.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminAssist.wpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Services = RegisterServices();
+        Application.Current.MainWindow = Services.GetRequiredService<MainWindow>();
+        Application.Current.MainWindow.Show();
+    }
+
+    /// <summary>
+    /// Gets the current <see cref="App"/> instance in use.
+    /// </summary>
+    public new static App Current => (App)Application.Current;
+
+    /// <summary>
+    /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+    /// </summary>
+    private IServiceProvider Services { get; }
+
+    private static IServiceProvider RegisterServices()
+    {
+        var services = new ServiceCollection();
+
+        services.RegisterViews();
+
+        return services.BuildServiceProvider();
+    }
+}
---
+++ AdminAssist.wpf/Services/ServiceConfigurationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdminAssist.wpf.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminAssist.wpf.Services;
+
+static class ServiceConfigurationExtensions
+{
+    /// <summary>
+    /// Registers the app services into the <paramref name="services"/> instance.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> in which to register the app services.</param>
+    public static void RegisterAppServices(this IServiceCollection services)
+    {
+    }
+
+    /// <summary>
+    /// Register the app views into the <paramref name="services"/> instance. Includes: <see cref="System.Windows.Window"/>,  <see cref="System.Windows.Controls.Page"/>, and other UI elements.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> in which to register the app views</param>
+    public static void RegisterViews(this IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+
+    /// <summary>
+    /// Registers the app viewModels into the <paramref name="services"/> instance.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> in which to register the app viewModels.</param>
+    public static void RegisterViewModels(this IServiceCollection services)
+    {
+    }
+}
