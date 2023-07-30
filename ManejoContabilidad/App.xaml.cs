5d8c614c5a986bee034b1ea9b4f0ddc0a1742bed
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-07-29 19:56:24 -0500

    .

---
+++ ManejoContabilidad/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ManejoContabilidad.Services;
+using ManejoContabilidad.Views.Windows;
+
+namespace ManejoContabilidad;
+
+
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    /// <summary>
+    ///     Gets the current <see cref="App" /> instance in use
+    /// </summary>
+    public new static App Current => (App)Application.Current;
+
+    /// <summary>
+    ///     Gets the <see cref="IServiceProvider" /> instance to resolve application services.
+    /// </summary>
+    public IServiceProvider Services { get; }
+
+    public App()
+    {
+        Services = ConfigureServices();
+
+        Application.Current.MainWindow = Services.GetRequiredService<MainWindow>();
+        Application.Current.MainWindow.Show();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        services.RegisterAppServices();
+        services.RegisterHelpers();
+        services.RegisterViews();
+        services.RegisterViewModels();
+
+        return services.BuildServiceProvider();
+    }
+}
---
+++ ManejoContabilidad/Services/ServiceConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManejoContabilidad.Views.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ManejoContabilidad.Services;
+
+internal static class ServiceConfigurationExtensions
+{
+    /// <summary>
+    /// Registers app services to the <paramref name="services"/> instance
+    /// </summary>
+    /// <param name="services">Service collection for Dependency Injection</param>
+    public static void RegisterAppServices(this IServiceCollection services)
+    {
+    }
+
+    public static void RegisterViewModels(this IServiceCollection services)
+    {
+    }
+
+    public static void RegisterViews(this IServiceCollection services)
+    {
+        // Windows
+        services.AddSingleton<MainWindow>();
+
+        // Pages
+    }
+
+    public static void RegisterHelpers(this IServiceCollection services)
+    {
+    }
+}
